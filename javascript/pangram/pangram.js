7b93fae97f4f89fe201585566d2a02fbc167a9af
Kelsey Hyde <kelseyhyde@gmail.com>
2017-07-15 10:33:39 +1200

    Added Pangram solution

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,19 @@
+var Pangram = function(sentence){
+  this.ascii = sentence.toLowerCase().split("").map(function(x) {
+   return x.charCodeAt(0);
+  });
+}
+
+Pangram.prototype.isPangram = function() {
+  if (this.ascii.length === 0){
+    return false;
+  }
+  for (var i = 97; i < 123; i++) {
+    if (this.ascii.indexOf(i) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = Pangram;
